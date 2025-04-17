62d85118b486101b558c39989c44ea4598eae598
raplowytee <raplowwytee@gmail.com>
2025-04-17 07:56:02 +0200

    minor changes innit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("send the flippin UI designs maine");
+        System.out.println("send the flipping UI designs maine");
     }
 }
